5b75600e92260f3a1505891a2eb1eabd13cccf14
sdupont06 <samuel.dupont1@icloud.com>
2025-04-13 03:35:49 -0400

    It now looks normal

--- NeedleDrop/app/_layout.tsx
+++ NeedleDrop/app/_layout.tsx
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
-import { NavigationContainer, TabActions } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import { useNavigation } from "@react-navigation/native";
-import HomeScreen from "./(tabs)/index";
 
 import LoginPage from "./login";
 import TabLayout from "./(tabs)/_layout";
@@ -17,8 +13,6 @@ export function setLog() {
 }
 
 export default function RootLayout() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false); // Replace with actual auth logic
-
   return (
     <Stack.Navigator>
       <Stack.Screen
